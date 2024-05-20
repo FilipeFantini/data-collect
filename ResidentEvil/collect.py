ec9a79b97e168c64c753c6a45e3394ab8d8bbe5c
Filipe Fantini <ffantini@enhesa.com>
2024-05-20 08:56:58 +0100

    1st operational version

--- ResidentEvil/collect.py
+++ ResidentEvil/collect.py
@@ -2,8 +2,8 @@
 
 import requests
 from bs4 import BeautifulSoup
-
-url = "https://www.residentevildatabase.com/personagens/ada-wong/"
+from tqdm import tqdm
+import pandas as pd
 
 headers = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
@@ -22,35 +22,69 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0',
 }
 
-resp = requests.get(url, headers=headers)
+def get_content(url):
+    resp = requests.get(url, headers=headers)
+    return resp
 
-# %%
-resp.status_code
+def get_basic_infos(soup):
+    div_page = soup.find("div", class_ = "td-page-content")
+    paragrafo = div_page.find_all("p")[1]
+    ems = paragrafo.find_all("em")
+    data = {}
+    for i in ems:
+        chave, valor, *_ = i.text.split(":")
+        chave = chave.strip(" ")
+        data[chave] = valor.strip(" ")
+    return data
 
-# %%
-soup = BeautifulSoup(resp.text)
-soup 
+def get_aparicoes(soup):
+    lis =  (soup.find("div", class_ = "td-page-content")
+                .find("h4")
+                .find_next()
+                .find_all("li"))
 
-# %%
-div_page = soup.find("div", class_ = "td-page-content")
-div_page
+    aparicoes = [i.text for i in lis]
+    return aparicoes
 
-# %%
-paragrafo = div_page.find_all("p")[1]
-paragrafo
+def get_personagem_infos(url):
+    resp = get_content(url)
 
-# %%
-ems = paragrafo.find_all("em")
-ems
+    if resp.status_code != 200:
+        print("Não foi possível obter os dados")
+        return{}
 
-# %%
-data = {}
-for i in ems:
-    chave, valor = i.text.split(":")
-    chave = chave.strip(" ")
-    data[chave] = valor.strip(" ")
+    else:
+        soup = BeautifulSoup(resp.text)
+        data = get_basic_infos(soup)
+        data["Aparicoes"] = get_aparicoes(soup)
+        return data
+    
+def get_links():
+    url = "https://www.residentevildatabase.com/personagens"
+    resp = requests.get(url, headers=headers)
+    soup_personagens = BeautifulSoup(resp.text)
+    ancoras = (soup_personagens.find("div", class_="td-page-content")
+                    .find_all("a"))
+    links = [i["href"] for i in ancoras]
+    return links
 
-data
+#%%
+links = get_links()
+data = []
+for i in tqdm(links):
+    d = get_personagem_infos(i)
+    d["link"] = i
+    nome = i.strip("/").split("/")[-1].replace("-"," ").title()
+    d["Nome"] = nome
+    data.append(d)
 
-# %%
-data["Altura"]
+#%%
+df = pd.DataFrame(data)
+df
+
+#%%
+df[~df["de nascimento"].isna()] #encontra o que gerou essa coluna nao esperada
+
+#%%
+df.to_parquet("dados_re.parquet", index=False)
+df.to_pickle("dados_re.pkl") #muito usado em ML
