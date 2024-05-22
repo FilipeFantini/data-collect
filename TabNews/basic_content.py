08cc9a93fc1d3efe8a1cffcdfa3290bb0da1d355
Filipe Fantini <ffantini@enhesa.com>
2024-05-22 08:06:53 +0100

    operational version

--- TabNews/basic_content.py
+++ TabNews/basic_content.py
@@ -4,6 +4,7 @@ import pandas as pd
 import datetime
 import json
 import os
+import time
 
 #%%
 def get_response(**kwargs):
@@ -25,13 +26,24 @@ def save_data(data, option='json'):
 
 #%%
 
-resp = get_response(page=1, per_page=100, strategy="new")
-if resp.status_code == 200:
-    print("sucesso!")
+page = 1 
+date_stop = pd.to_datetime('2024-03-01').date()
+while True:
+    print(page)
 
-#%%
-data = resp.json()
-data
+    resp = get_response(page=page, per_page=20, strategy="new")
+    if resp.status_code == 200:
+        data = resp.json()
+        save_data(data)
 
-#%%
-save_data(data)
+        date = pd.to_datetime(data[-1]["updated_at"])
+        if len(data) < 100 or date < date_stop:
+            break
+
+        page += 1
+        time.sleep(1)
+
+    else:
+        print(resp.status_code)
+        print(resp.json())
+        time.sleep(10)
