f90e4bb16d24b1a5e8280e5a4db3c1a6449f9d16
FilipeFantini <FilipeFantini>
2024-06-24 21:13:35 +0000

    added unity catalog and dynamic flatten json
    thanks A LOT to https://github.com/OlaJaner/Flatten_json

--- Weather/weather_get.py
+++ Weather/weather_get.py
@@ -11,13 +11,13 @@ api_key = config.get('API', 'weather_api_key')
 import requests 
 from datetime import datetime, timedelta
 import json
-import os
+import time
 
-directory = "/dbfs/FileStore/datalake/weather/"
-os.makedirs(directory, exist_ok=True)
+directory = "/Volumes/dev/raw/weather/"
 base = "https://api.weatherapi.com/v1/history.json?"
-date = (datetime.today() - timedelta(days=7)).strftime('%Y-%m-%d')
-cities = ["Lisbon", "London", "Paris", "Sao Paulo", "Rio de Janeiro", "Tokyo", "California"]
+date = (datetime.today() - timedelta(days=14)).strftime('%Y-%m-%d')
+cities = ["New York City", "London", "Tokyo", "Paris", "Hong Kong", "Singapore", "Shanghai", "Beijing", "Los Angeles", "Dubai", "Sydney", "San Francisco", "Chicago", "Berlin", "Moscow", "Toronto", "Seoul", "Mumbai", "São Paulo", "Mexico City", "Buenos Aires", "Bangkok", "Istanbul", "Delhi", "Jakarta", "Cairo", "Kuala Lumpur", "Johannesburg", "Frankfurt", "Madrid", "Amsterdam", "Zurich", "Brussels", "Vienna", "Dublin", "Milan", "Stockholm", "Copenhagen", "Oslo", "Helsinki", "Warsaw", "Lisbon", "Santiago", "Manila", "Lima", "Bogota", "Rio de Janeiro", "Caracas", "Cape Town", "Melbourne", "Barcelona", "Vancouver", "Montreal", "Washington", "Rome","California", "Porto", "Salvador","Campinas", "Malmo","Sao Paulo"]
+
 
 for city in cities:
     url = f"{base}q={city}&dt={date}"
@@ -27,6 +27,7 @@ for city in cities:
 
     resp = requests.get(url, params=params)
     print(f"{city}, {resp.status_code}")
+    time.sleep(1)
 
     data = resp.json()
     now = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
