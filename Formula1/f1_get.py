285bb66eebfb5876fab7e6461c43271f203de33a
FilipeFantini <FilipeFantini>
2024-07-23 20:54:46 +0000

    partial addition of car_details

--- Formula1/f1_get.py
+++ Formula1/f1_get.py
@@ -1,4 +1,11 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC ## Get F1 Data
+# MAGIC And save it in raw layer for further data cleaning and transformation using Medalion Architecture.
+# MAGIC More details on the source data at: https://openf1.org/#introduction
+
+# COMMAND ----------
+
 import requests
 import json
 import datetime
@@ -32,11 +39,23 @@ def get_and_save_json(url, dimension, extra_info=""):
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ###Get Meetings 
+# MAGIC Meeting = Grand Prix, or testing event
+
+# COMMAND ----------
+
 #get meetings
 get_and_save_json(f"{base}meetings", "meetings")
 
 
 #get_data(f"{base}sessions", "sessions")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Get Sessions 
+# MAGIC Based on current Meetings' already available data
 
 # COMMAND ----------
 
@@ -47,7 +66,22 @@ list_meetings = list(df.select(df.meeting_key).toPandas()["meeting_key"])
 
 for i in list_meetings:
     get_and_save_json(f"{base}sessions?meeting_key={i}", "sessions", f"mkey{i}")
-    print(i)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Get Car Details 
+# MAGIC Based on current Meetings and Sessions already available
+
+# COMMAND ----------
+
+#get car details
+sessions = "/Volumes/raw/formula1/sessions/"
+list_sessions = list(spark.read.json(sessions).toPandas()["session_key"])
+
+#breaking because it's timing out from source, I need to paginate it by driver and session key, too much data
+for i in list_sessions:
+    get_and_save_json(f"{base}car_data?session_key={i}", "car_data", f"skey{i}")
 
 # COMMAND ----------
 
