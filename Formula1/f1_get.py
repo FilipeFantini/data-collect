0dcba88efcd7fe9743248f39997683204623b028
FilipeFantini <FilipeFantini>
2024-07-30 21:10:03 +0000

    added load_logs
    In order to keep history of what was loaded already in case some process breaks in the middle of a full load - car_details, for instance

--- Formula1/f1_get.py
+++ Formula1/f1_get.py
@@ -8,7 +8,11 @@
 
 import requests
 import json
-import datetime
+from datetime import datetime
+from pyspark.sql import Row
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType
+from pyspark.sql.functions import current_timestamp, to_timestamp
+
 
 base = "https://api.openf1.org/v1/"
 
@@ -26,16 +30,39 @@ def get_and_save_parquet(url, dimension,  extra_info=""):
         df.coalesce(1).write.mode("overwrite").parquet(path)
     else: print(response.json())
 
-def get_and_save_json(url, dimension, extra_info=""):
-    response = requests.get(url)
-    data = response.json()
+def get_and_save_json(url, dimension, *args):
+    try:
+        response = requests.get(url)
+    except:
+        print(f"Did not work for url: {url}")
+    extra_path = args[0]
     if response.status_code == 200:
-        now = datetime.datetime.now().strftime("%Y-%m")
-        filename = f"{dimension}{extra_info}_{now}"
+        data = response.json()
+        now = datetime.now().strftime("%Y-%m")
+        filename = f"{dimension}{extra_path}_{now}"
         path = f"/Volumes/raw/formula1/{dimension}/{filename}.json"
         with open(path, "w") as open_file:
             json.dump(data, open_file)
     else: print(response.json())
+
+    checkpoint = {
+        "project": "F1",
+        "dimension": f"{dimension}",
+        "url": f"{url}",
+        "path": f"{path}",
+        "created": f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}",
+        "total_rows": f"{len(data)}"
+        #datetime.strptime(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S')
+    }
+
+    path_log = f"/Volumes/raw/metadata/load_logs/logs_{filename}.json"
+    with open(path_log, "w") as open_file_logs:
+        json.dump(checkpoint, open_file_logs)
+
+
+# COMMAND ----------
+
+df2 = spark.read.json("/Volumes/raw/metadata/load_logs/").display()
 
 # COMMAND ----------
 
@@ -70,6 +97,22 @@ for i in list_meetings:
 # COMMAND ----------
 
 # MAGIC %md
+# MAGIC ## Get Drivers
+# MAGIC Based on current Meetings already available
+
+# COMMAND ----------
+
+#get drivers
+path = "/Volumes/raw/formula1/meetings/"
+df = spark.read.json(path)
+list_meetings = list(df.select(df.meeting_key).toPandas()["meeting_key"])
+
+for i in list_meetings:
+    get_and_save_json(f"{base}drivers?meeting_key={i}", "drivers", f"_mkey{i}")
+
+# COMMAND ----------
+
+# MAGIC %md
 # MAGIC ###Get Car Details 
 # MAGIC Based on current Meetings and Sessions already available
 
@@ -78,10 +121,17 @@ for i in list_meetings:
 #get car details
 sessions = "/Volumes/raw/formula1/sessions/"
 list_sessions = list(spark.read.json(sessions).toPandas()["session_key"])
+drivers = "/Volumes/raw/formula1/drivers/"
+list_drivers = list(spark.read.json(drivers).toPandas()["driver_number"].drop_duplicates())
 
 #breaking because it's timing out from source, I need to paginate it by driver and session key, too much data
-for i in list_sessions:
-    get_and_save_json(f"{base}car_data?session_key={i}", "car_data", f"skey{i}")
+
+for i in list_drivers:
+    for j in list_sessions:
+        for n_gear in range(0,9):
+            try:
+                get_and_save_json(f"{base}car_data?driver_number={i}&session_key={j}&n_gear={n_gear}", "car_data", f"_skey{j}_dn{i}_ng{n_gear}")
+            except: continue
 
 # COMMAND ----------
 
